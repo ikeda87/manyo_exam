bc80fba5e96285416bdb8277bc3a7f11d7978ddb
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-17 17:30:07 +0000

    spec/factories/tasks.rb

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,24 +1,22 @@
 FactoryBot.define do
   factory :task do
-    title { 'Factoryで作ったデフォルトのタイトル2' }
-    content { 'Factoryで作ったデフォルトのコンテンツ1' }
-    deadline { '002021-01-10' }
+    title { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
+    deadline { '002020-11-25' }
     status { '未着手' }
     priority { '高' }
   end
-
   factory :second_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル2' }
-    content { 'Factoryで作ったデフォルトのコンテンツ2' }
-    deadline { '002021-01-11' }
+    title { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
+    deadline { '002020-11-30' }
     status { '完了' }
     priority { '中' }
   end
-
   factory :third_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル3' }
-    content { 'Factoryで作ったデフォルトのコンテンツ3' }
-    deadline { '002021-01-12' }
+    title { 'Factoryで作ったデフォルトのタイトル３' }
+    content { 'Factoryで作ったデフォルトのコンテント３' }
+    deadline { '002020-12-20' }
     status { '着手中' }
     priority { '低' }
   end
