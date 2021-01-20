593f4d0c1f56b8cf1400834a37556f53691bfad3
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-20 14:12:34 +0000

    update views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to @task, notice: 'success'
+      redirect_to @task, notice: '詳細が表示されます'
     else
       render :new
     end
