f85c6240270ecbefc9f8e8e2512f0009d0a5f3b7
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-21 03:28:13 +0000

    edit task spec

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'タスク管理機能', type: :system do
     click_button 'Log in'
     visit tasks_path
   end
-
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -27,18 +26,14 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
-        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
-      end
-    end
-
-    context 'タスクが作成日時の降順で並んでいた場合' do
-      it '新しいタスクが一番上に表示される' do
-        task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル３'
+  describe 'タスク管理機能', type: :system do
+    describe '一覧表示機能' do
+      context '一覧画面に遷移した場合' do
+        it '作成済みのタスク一覧が表示される' do
+          task = FactoryBot.create(:task, title: 'task')
+          visit tasks_path
+          expect(page).to have_content 'task_failure'
+        end
       end
     end
   end
