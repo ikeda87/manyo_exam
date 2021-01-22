c227a2ec30ad9a7b92d1161b3e056c2dbb9c3b6e
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-22 08:44:55 +0000

    add end

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,18 +6,22 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
       end
     end
+
     context 'タスクが作成日時の降順で並んでいた場合' do
       it '新しいタスクが一番上に表示される' do
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル３'
       end
     end
+  end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
