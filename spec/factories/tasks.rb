2d6c9bbadfe3698185c9f599bcaf1350fe0b68a8
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-24 17:20:35 +0000

    add spec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,16 @@ FactoryBot.define do
   factory :task do
     title { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
-    end
-
+    status { '未着手' }
+  end
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル２' }
     content { 'Factoryで作ったデフォルトのコンテント２' }
-    end
+    status { '完了' }
+  end
+  factory :third_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル３' }
+    content { 'Factoryで作ったデフォルトのコンテント３' }
+    status { '着手中' }
+  end
 end
