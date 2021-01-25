d19c69bcdac29046b5696c1d4b14fb056d0a9725
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-25 08:33:03 +0000

    add route resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only:[:new, :create, :show]
   resources :sessions, only:[:new, :create, :destroy]
   namespace :admin do
-    end
+    resources :users
+  end
   resources :labels,only:[:new, :create, :destroy]
 end
