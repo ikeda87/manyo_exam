33ba62603a306565ce781a43c6564b706ccff180
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-26 13:49:14 +0000

    add task label

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,17 +1,23 @@
 FactoryBot.define do
   factory :task do
-    title { 'Factoryで作ったデフォルトのタイトル1' }
-    content { 'Factoryで作ったデフォルトのコンテント1' }
+    title { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
+    deadline { '002021-02-25' }
     status { '未着手' }
+    priority { '高' }
   end
   factory :second_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル2' }
-    content { 'Factoryで作ったデフォルトのコンテント2' }
+    title { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
+    deadline { '002021-02-26' }
     status { '完了' }
+    priority { '中' }
   end
   factory :third_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル3' }
-    content { 'Factoryで作ったデフォルトのコンテント3' }
+    title { 'Factoryで作ったデフォルトのタイトル３' }
+    content { 'Factoryで作ったデフォルトのコンテント３' }
+    deadline { '002021-02-27' }
     status { '着手中' }
+    priority { '低' }
   end
 end
