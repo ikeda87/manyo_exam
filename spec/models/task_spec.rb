a5cd2ac123ec7b79cd7d3534944a1a276c316cdc
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-26 13:47:16 +0000

    add model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,52 +1,26 @@
 require 'rails_helper'
 describe 'タスクモデル機能', type: :model do
-  describe 'バリデーションのテスト' do
-    context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
-        expect(task).not_to be_valid
-      end
-    end
-    context 'タスクの詳細が空の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(title: '失敗テスト', content: '')
-        expect(task).not_to be_valid
-      end
-    end
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
-      it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト')
-        expect(task).to be_valid
-      end
-    end
-  end
   describe '検索機能' do
-    # let!(:task){ FactoryBot.create(:task, title: 'task',status: '完了') }
-    # let!(:second_task){ FactoryBot.create(:second_task, title: 'sample', status:'完了') }
-    before do
-      @user = FactoryBot.create(:fifth_user)
-      @task = FactoryBot.create(:task,title: 'task',status: '完了', user: @user)
-      @second_task = FactoryBot.create(:second_task,title: 'sample', status: '着手中', user: @user)
-      @third_task = FactoryBot.create(:third_task,title: 'example', status: '着手中', user: @user)
-    end
-    context 'scopeメソッドでタイトル検索をした場合' do
-      it '検索ワードを含んだタスクが絞られる'do
+    let!(:task) { FactoryBot.create(:task, title: 'task') }
+    let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
         expect(Task.search_title('task')).to include(@task)
         expect(Task.search_title('task')).not_to include(@second_task)
         expect(Task.search_title('task')).not_to include(@third_task)
         expect(Task.search_title('task').count).to eq 1
       end
     end
-    context 'scopeメソッドでステータスを検索した場合' do
-      it '検索されたステータスのタスクが絞られる' do
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
         expect(Task.search_status('着手中')).not_to include(@task)
         expect(Task.search_status('着手中')).to include(@second_task)
         expect(Task.search_status('着手中')).to include(@third_task)
         expect(Task.search_status('着手中').count).to eq 2
       end
     end
-    context 'scopeメソッドでタイトルとステータスを入力して検索した場合' do
-      it '検索ワードがタイトルに含まれ、かつ選択されたステータスが一致するタスクが表示される' do
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
         expect(Task.search_title('task').search_status('完了')).to include(@task)
         expect(Task.search_title('task').search_status('完了')).not_to include(@second_task)
         expect(Task.search_title('task').search_status('完了')).not_to include(@third_task)
