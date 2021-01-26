ecb5161ca44c1089ea0d3ceb12c7d81bbf4d81e1
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-26 10:20:06 +0000

    add task

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１'
+        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル1'
       end
     end
     # テスト内容を追加で記載する
     context 'タスクが作成日時の降順で並んでいた場合' do
       it '新しいタスクが一番上に表示される' do
         task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル３'
+        expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル3'
       end
     end
   end
@@ -33,28 +33,29 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '検索機能' do
-      before do
-        # 必要に応じて、テストデータの内容を変更して構わない
-        FactoryBot.create(:task, title: "task")
-        FactoryBot.create(:second_task, title: "sample")
-      end
+    before do
+      visit tasks_path
+    end
       context 'タイトルであいまい検索をした場合' do
         it "検索キーワードを含むタスクで絞り込まれる" do
-          visit tasks_path
-          # タスクの検索欄に検索ワードを入力する (例: task)
-          # 検索ボタンを押す
-          expect(page).to have_content 'task'
+          fill_in "search_textarea", with:"タイトル2"
+          click_button '検索'
+          expect(page).to have_content 'タイトル2'
         end
       end
       context 'ステータス検索をした場合' do
         it "ステータスに完全一致するタスクが絞り込まれる" do
-          # ここに実装する
-          # プルダウンを選択する「select」について調べてみること
+          select "未着手", from: "ステータス"
+          click_button '検索'
+          expect(page).to have_content '未着手'
         end
       end
       context 'タイトルのあいまい検索とステータス検索をした場合' do
         it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-          # ここに実装する
+          fill_in "search_textarea", with:"タイトル1"
+          select "未着手", from: "ステータス"
+          click_button '検索'
+          expect(page).to have_content 'Factoryで作ったデフォルトのタイトル1'
         end
       end
     end
